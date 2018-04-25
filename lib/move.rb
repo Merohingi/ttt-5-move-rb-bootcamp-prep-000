76b56e8ef124bdb1a1a83ec7718951e8e783cd07
Marvin Bonds <mbonds1@mail.depaul.edu>
2018-04-25 05:27:39 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,5 +1,5 @@
 def display_board(board)
-  puts "#{board[0]} | #{board[1]} | #{board[2]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
@@ -11,7 +11,7 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-def move(board,position,char = '')
+def move(board,position,char = 'X')
   board[position] << char
 
 end
